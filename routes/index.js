f4ddbf2cab10e9b7110179111b1d21a212ff9340
Ashesh Singh <asing80@uic.edu>
2020-11-18 02:43:50 -0600

    Update attack pages and routes

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,18 @@ router.get(
   (req, res) => {
   // eslint-disable-next-line node/no-deprecated-api
     const parsedUrl = url.parse(req.protocol + '://' + req.get('host') + req.originalUrl, true)
+
+    function setCookies () {
+      const now = new Date().getTime()
+      const maxAge = 2 * 60 * 60 * 1000
+      res.cookie('serverCookie', now, { maxAge: maxAge })
+      res.cookie('serverCookieHttpOnly', now, { httpOnly: true, maxAge: maxAge })
+      res.cookie('serverCookieSameSite', now, { sameSite: 'strict', maxAge: maxAge })
+      res.cookie('serverCookieSecure', now, { secure: true, maxAge: maxAge })
+    }
+
+    if (process.env.MODE === 'vastavik') setCookies()
+
     res.render('index', { title: 'Welcome', parsedUrl: parsedUrl })
   })
 
@@ -24,16 +36,14 @@ router.post(
   '/home',
   (req, res) => {
     const parsedUrl = url.parse(req.protocol + '://' + req.get('host') + req.originalUrl, true)
-    const randomInt = Math.floor(Math.random() * 100)
-    res.render('pages/home', { title: 'Home', parsedUrl: parsedUrl, randomInt: randomInt })
+    const username = req.body.username ? req.body.username : ''
+    res.render('pages/home', { title: 'Home', parsedUrl: parsedUrl, username: username })
   })
-
 router.get(
   '/home',
   (req, res) => {
     const parsedUrl = url.parse(req.protocol + '://' + req.get('host') + req.originalUrl, true)
-    const randomInt = Math.floor(Math.random() * 100)
-    res.render('pages/home', { title: 'Home', parsedUrl: parsedUrl, randomInt: randomInt })
+    res.render('pages/home', { title: 'Home', parsedUrl: parsedUrl, username: '' })
   })
 
 router.get(
@@ -46,28 +56,18 @@ router.get(
 router.get(
   '/:category/:name',
   (req, res, next) => {
-    function setCookies () {
-      const now = new Date().getTime()
-      const maxAge = 2 * 60 * 60 * 1000
-      res.cookie('serverCookie', now, { maxAge: maxAge })
-      res.cookie('serverCookieHttpOnly', now, { httpOnly: true, maxAge: maxAge })
-      res.cookie('serverCookieSameSite', now, { sameSite: 'strict', maxAge: maxAge })
-      res.cookie('serverCookieSecure', now, { secure: true, maxAge: maxAge })
-    }
-
     function hasAccess (path) {
       fs.access(path, (err) => { return !err })
     }
 
-    if (hasAccess(`views/pages/${req.params.category}s/${req.params.name}`)) {
+    if (hasAccess(`views/pages/${req.params.category}s/${req.params.name}`) &&
+        process.env.MODE === 'kalpanik') {
       next(createError(404))
     } else {
       const parsedUrl = url.parse(req.protocol + '://' + req.get('host') + req.originalUrl, true)
       const reqHeaders = req.headers
       const serverFingerprint = req.fingerprint
       const requestCookies = req.cookies
-
-      setCookies()
 
       res.render(`pages/${req.params.category}s/${req.params.name}/`, {
         title: req.params.name,
