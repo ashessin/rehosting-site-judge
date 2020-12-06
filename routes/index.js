eed4a26ac3119a9e0fc7cc4b8fd178c058adfa72
Ashesh Singh <asing80@uic.edu>
2020-12-06 04:24:12 -0600

    Rearrange code

--- routes/index.js
+++ routes/index.js
@@ -5,78 +5,59 @@ const url = require('url')
 const fs = require('fs')
 
 /* GET home page. */
-router.get(
-  '/',
-  (req, res) => {
+router.get('/', (req, res) => {
+  if (process.env.MODE === 'vastavik') setCookies(res)
+  res.render('index', { title: 'Welcome', url: parseUrl(req) })
+})
+
+router.get('/location', (req, res) => {
+  res.render('pages/location', { title: 'Location', url: parseUrl(req) })
+})
+router.get('/login', (req, res) => {
+  res.render('pages/login', { title: 'Login', url: parseUrl(req) })
+})
+router.post('/home', (req, res) => {
+  const username = req.body.username ? req.body.username : ''
+  res.render('pages/home', { title: 'Home', url: parseUrl(req), username: username })
+})
+router.get('/home', (req, res) => {
+  res.render('pages/home', { title: 'Home', url: parseUrl(req), username: '' })
+})
+
+router.get('/:category/:name', (req, res, next) => {
+  if (hasAccess(`views/pages/${req.params.category}s/${req.params.name}`)) {
+    next(createError(404))
+  } else {
+    const reqHeaders = req.headers
+    const serverFingerprint = req.fingerprint
+    const requestCookies = req.cookies
+    const data = {
+      title: req.params.name,
+      url: parseUrl(req),
+      requestHeaders: reqHeaders,
+      requestFingerprint: serverFingerprint,
+      requestCookies: requestCookies
+    }
+    res.render(`pages/${req.params.category}s/${req.params.name}/`, data)
+  }
+})
+
+function setCookies (res) {
+  const now = new Date().getTime()
+  const maxAge = 2 * 60 * 60 * 1000
+  res.cookie('serverCookie', now, { maxAge: maxAge })
+  res.cookie('serverCookieHttpOnly', now, { httpOnly: true, maxAge: maxAge })
+  res.cookie('serverCookieSameSite', now, { sameSite: 'strict', maxAge: maxAge })
+  res.cookie('serverCookieSecure', now, { secure: true, maxAge: maxAge })
+}
+
+function hasAccess (path) {
+  fs.access(path, (err) => { return !err })
+}
+
+function parseUrl (req) {
   // eslint-disable-next-line node/no-deprecated-api
-    const parsedUrl = url.parse(req.protocol + '://' + req.get('host') + req.originalUrl, true)
-
-    function setCookies () {
-      const now = new Date().getTime()
-      const maxAge = 2 * 60 * 60 * 1000
-      res.cookie('serverCookie', now, { maxAge: maxAge })
-      res.cookie('serverCookieHttpOnly', now, { httpOnly: true, maxAge: maxAge })
-      res.cookie('serverCookieSameSite', now, { sameSite: 'strict', maxAge: maxAge })
-      res.cookie('serverCookieSecure', now, { secure: true, maxAge: maxAge })
-    }
-
-    if (process.env.MODE === 'vastavik') setCookies()
-
-    res.render('index', { title: 'Welcome', parsedUrl: parsedUrl })
-  })
-
-router.get(
-  '/login',
-  (req, res) => {
-    const parsedUrl = url.parse(req.protocol + '://' + req.get('host') + req.originalUrl, true)
-    res.render('pages/login', { title: 'Login', parsedUrl: parsedUrl })
-  })
-
-router.post(
-  '/home',
-  (req, res) => {
-    const parsedUrl = url.parse(req.protocol + '://' + req.get('host') + req.originalUrl, true)
-    const username = req.body.username ? req.body.username : ''
-    res.render('pages/home', { title: 'Home', parsedUrl: parsedUrl, username: username })
-  })
-router.get(
-  '/home',
-  (req, res) => {
-    const parsedUrl = url.parse(req.protocol + '://' + req.get('host') + req.originalUrl, true)
-    res.render('pages/home', { title: 'Home', parsedUrl: parsedUrl, username: '' })
-  })
-
-router.get(
-  '/location',
-  (req, res) => {
-    const parsedUrl = url.parse(req.protocol + '://' + req.get('host') + req.originalUrl, true)
-    res.render('pages/location', { title: 'Location', parsedUrl: parsedUrl })
-  })
-
-router.get(
-  '/:category/:name',
-  (req, res, next) => {
-    function hasAccess (path) {
-      fs.access(path, (err) => { return !err })
-    }
-
-    if (hasAccess(`views/pages/${req.params.category}s/${req.params.name}`) &&
-        process.env.MODE === 'kalpanik') {
-      next(createError(404))
-    } else {
-      const parsedUrl = url.parse(req.protocol + '://' + req.get('host') + req.originalUrl, true)
-      const reqHeaders = req.headers
-      const serverFingerprint = req.fingerprint
-      const requestCookies = req.cookies
-
-      res.render(`pages/${req.params.category}s/${req.params.name}/`, {
-        title: req.params.name,
-        parsedUrl: parsedUrl,
-        requestHeaders: reqHeaders,
-        requestFingerprint: serverFingerprint,
-        requestCookies: requestCookies
-      })
-    }
-  })
+  return url.parse(req.protocol + '://' + req.get('host') + req.originalUrl, true)
+}
 
 module.exports = router
